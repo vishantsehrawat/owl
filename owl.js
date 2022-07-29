f6343d565931e8821be3fb8469a44db6bdc3ef8c
vishant <vishant.96.sehrawat@gmail.com>
2022-07-29 23:11:46 +0530

    completed with some hiccups

--- owl.js
+++ owl.js
@@ -1,16 +1,17 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    // margin: 10,
+    nav: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
         },
-        600:{
-            items:3
+        600: {
+            items: 2,
         },
-        1000:{
-            items:5
-        }
-    }
-})
+        1000: {
+            items: 4,
+        },
+    },
+});
